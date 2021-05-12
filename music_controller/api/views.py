3c87dd997dd746a96b689e20d678683906e0bbd5
bjarki <bjarki@inecta.com>
2021-05-12 11:35:18 -0300

    Add create room api

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -28,9 +28,12 @@ class CreateRoomView(APIView):
                 room = queryset[0]
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
-                room.save(update_fields=['votes_to_skip', 'guest_can_pause'])
+                room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
             else:
-                room = Room(host=host, votes_to_skip=votes_to_skip,
-                            guest_can_pause=guest_can_pause)
+                room = Room(host=host, guest_can_pause=guest_can_pause,
+                            votes_to_skip=votes_to_skip)
                 room.save()
-        return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+                return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
