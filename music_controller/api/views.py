98d8f41b649237c614c61e5d5320de18c56569f8
bjarki <bjarki@inecta.com>
2021-05-12 09:50:16 -0300

    Add api listview

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -3,6 +3,6 @@ from rest_framework import generics
 from .serializers import RoomSerializer
 from .models import Room
 
-class RoomView(generics.CreateAPIView):
+class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
