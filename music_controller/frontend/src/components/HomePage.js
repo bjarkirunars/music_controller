204006029f15431b330bfb916759de18464ffa4a
bjarki <bjarki@inecta.com>
2021-05-31 19:17:16 -0300

    Finish #11

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -17,6 +17,7 @@ export default class Home extends Component {
     this.state = {
       roomCode: null,
     };
+    this.clearRoomCode = this.clearRoomCode.bind(this);
   }
 
   async componentDidMount() {
@@ -25,6 +26,10 @@ export default class Home extends Component {
       .then((data) => {
         this.setState({ roomCode: data.code });
       });
+  }
+
+  clearRoomCode() {
+    this.setState({ roomCode: null });
   }
 
   renderHomePage() {
@@ -66,7 +71,12 @@ export default class Home extends Component {
           />
           <Route path="/join" component={JoinRoomPage} />
           <Route path="/create" component={CreateRoomPage} />
-          <Route path="/room/:roomCode" component={Room} />
+          <Route
+            path="/room/:roomCode"
+            render={(props) => {
+              return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+            }}
+          />
         </Switch>
       </Router>
     );
